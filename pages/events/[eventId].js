46f8d3ea2ec1257a9cb8536c8077d2e927e870d5
Puspita Vabna <sharif7761@gmail.com>
2022-05-09 00:48:16 +0600

    pages setup

---
+++ pages/events/[eventId].js
@@ -0,0 +1,8 @@
+export default function EventDetailsPage() {
+    return (
+        <div>
+            <h1>Event Detail Page</h1>
+        </div>
+    )
+}
+
---
+++ pages/events/index.js
@@ -0,0 +1,8 @@
+export default function AllEventsPage() {
+    return (
+        <div>
+            <h1>All Events Page</h1>
+        </div>
+    )
+}
+
